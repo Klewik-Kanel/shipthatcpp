3c3e55db1f2ce4e4e4b99210413ffc9680ada01a
Klewik-Kanel <kaustubhpandey812@gmail.com>
2026-07-17 06:27:48 +0530

    lesson 12

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
     vector<int> nums;
     int x{0};
     while(cin >> x) nums.push_back(x);
-    long long total = accumulate(nums.begin(), nums.end(),1LL,[](long long acc, int x){
+    long long total = accumulate(nums.begin(), nums.end(),0LL,[](long long acc, int x){
         return x%2 == 0 ? acc + 1LL*x*x : acc;
     });
     cout<< total;
