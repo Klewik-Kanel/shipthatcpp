06d3f536b9eca85fc420227bded6343335b2a9bf
Klewik-Kanel <kaustubhpandey812@gmail.com>
2026-07-17 06:26:44 +0530

    lesson 12

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 #include <map>
 #include <unordered_set>
 
 using namespace std;
 
 int main() {
-    unordered_set<string> seen;
-    string s;
-    while(cin >> s) seen.insert(s);
-    cout<< seen.size();
+    vector<int> nums;
+    int x{0};
+    while(cin >> x) nums.push_back(x);
+    long long total = accumulate(nums.begin(), nums.end(),1LL,[](long long acc, int x){
+        return x%2 == 0 ? acc + 1LL*x*x : acc;
+    });
+    cout<< total;
+    return 0;
 }
